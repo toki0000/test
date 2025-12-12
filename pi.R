3cccd96174a3ca47e1286431b335e1ebce05fbb5
toki0000 <835883401@qq.com>
2025-12-12 11:06:03 +0900

    Create pi.R
    Estimate pi via Monte Carlo

---
+++ pi.R
@@ -0,0 +1,8 @@
+# For fun only. 
+# See: https://academo.org/demos/estimating-pi-monte-carlo/
+
+Nreps <- 1000000
+x     <- runif(Nreps, -1, 1)
+y     <- runif(Nreps, -1, 1)
+mean(x^2 + y^2 < 1)
+  
